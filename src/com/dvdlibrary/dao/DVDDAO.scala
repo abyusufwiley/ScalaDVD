6456aaa57d77445ae6307a8738f65e675ee87085
Nabin Ale <nale@wiley.com>
2024-04-17 16:23:48 +0100

    DVDDAO trait completed

--- src/com/dvdlibrary/dao/DVDDAO.scala
+++ src/com/dvdlibrary/dao/DVDDAO.scala
@@ -1,5 +1,14 @@
 package com.dvdlibrary.dao
 
+import com.dvdlibrary.dto.DVD
+
 trait DVDDAO {
+  def addDvd(dvdName: String, dvd: DVD): DVD
+
+  def getAllDvds: List[DVD]
+
+  def getDvd(dvdName: String): DVD
+
+  def removeDvd(dvdName: String): DVD
 
 }
